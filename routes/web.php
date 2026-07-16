1e75f7279e299d40d249a47354fa2695eb9911cb
novan <ofcialnovan@gmail.com>
2026-07-16 15:46:53 +0700

    tambah migrasi tabel produk mart

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
 Route::get('/belajar', function () {
     return view('belajar');
+});
+
+Route::get('/dashboard', function () {
+    return 'Halo Tio, Route Dashboard Aman!';
 });
