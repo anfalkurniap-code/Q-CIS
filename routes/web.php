5f48c57950c623a30a5f342ec44694134704667b
novan <ofcialnovan@gmail.com>
2026-07-17 11:03:08 +0700

    Menghapus kode/file yang tidak digunakan

--- routes/web.php
+++ routes/web.php
@@ -5,19 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-<<<<<<< HEAD
 
-Route::get('/belajar', function () {
-    return view('belajar');
+Route::get('/loginkasir', function () {
+    return view('loginkasir');
 });
 
-Route::get('/dashboard', function () {
-    return 'Halo Tio, Route Dashboard Aman!';
-=======
-Route::get('/loginKasir', function () {
-    return view('loginKasir');
+Route::get('/Tampilanawallogin', function () {
+    return view('Tampilanawallogin');
 });
-Route::get('/TampilanAwalLogin', function(){
-    return view('TampilanAwalLogin');
->>>>>>> 07a3824b7b11682a125de8ad0862fa4f70197576
-});
+
