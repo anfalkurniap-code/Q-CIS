edca785396412615a77ca7b6dee4f761be4b40c4
anfalkurniap <anfalkurnip@gmail.com>
2026-07-17 16:43:48 +0700

    feat: menambah user kasir baru via tinker dan memperbaiki visual error border pada form login menambahkan halaman depan kasir serta menambahkan AuthKasirController

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,28 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthKasirController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return 'Halo Tio, Route Dashboard Aman!';
+Route::get('/TampilanAwalLogin', function () {
+    return view('TampilanAwalLogin');
 });
 
 Route::get('/loginKasir', function () {
     return view('loginKasir');
-});
+})->name('login');
 
-Route::get('/TampilanAwalLogin', function () {
-    return view('TampilanAwalLogin');
+Route::post('/loginKasir/proses', [AuthKasirController::class, 'login'])->name('login.post');
+
+Route::middleware(['auth'])->group(function () {
+
+Route::get('/HalamanDepanKasir', function () {
+    return view('HalamanDepanKasir');
+})->name('dashboard.kasir');
+
+Route::post('/logoutKasir', [AuthKasirController::class, 'logout'])->name('logout');
+
 });
