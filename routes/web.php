7fee1b45284d7fd35544c7a96f75f893d306922d
anfalkurniap <anfalkurnip@gmail.com>
2026-07-17 11:16:43 +0700

    Menghapus route belajar dan merapikan web.php

--- routes/web.php
+++ routes/web.php
@@ -5,19 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-<<<<<<< HEAD
-
-Route::get('/belajar', function () {
-    return view('belajar');
-});
 
 Route::get('/dashboard', function () {
     return 'Halo Tio, Route Dashboard Aman!';
-=======
+});
+
 Route::get('/loginKasir', function () {
     return view('loginKasir');
 });
-Route::get('/TampilanAwalLogin', function(){
+
+Route::get('/TampilanAwalLogin', function () {
     return view('TampilanAwalLogin');
->>>>>>> 07a3824b7b11682a125de8ad0862fa4f70197576
 });
